86221ab073400799af8b4148f0ad0c372a130890
Travis Swicegood <development@domain51.com>
2011-05-05 18:08:59 -0500

    simplify to use new armstrong.dev tasks

--- fabfile/__init__.py
+++ fabfile/__init__.py
@@ -1,4 +1,4 @@
-from ._utils import *
+from armstrong.dev.tasks import *
 
 
 settings = {
@@ -20,49 +20,7 @@ settings = {
     'ARMSTRONG_EXTERNAL_VIDEO_BACKEND': 'armstrong.core.arm_content.video.backends.YouTubeBackend'
 }
 
-
-@task
-def clean():
-    local('find . -name "*.py[co]" -exec rm {} \;')
+main_app = "embedded_video"
+tested_apps = ("arm_content_support", "arm_content", main_app, )
 
 
-@task
-def pep8():
-    local('find ./armstrong -name "*.py" | xargs pep8', capture=False)
-
-
-@task
-def test():
-    with html_coverage_report():
-        run_tests(settings, 'arm_content_support', 'arm_content', 'embedded_video')
-
-
-@task
-def syncdb():
-    from d51.django.virtualenv.base import VirtualEnvironment
-    runner = VirtualEnvironment()
-    runner.run(settings)
-    runner.call_command("syncdb")
-    runner.call_command("migrate")
-
-@task
-def runserver():
-    from d51.django.virtualenv.base import VirtualEnvironment
-    runner = VirtualEnvironment()
-    runner.run(settings)
-    runner.call_command("runserver")
-
-
-@task
-def shell():
-    from d51.django.virtualenv.base import VirtualEnvironment
-    runner = VirtualEnvironment()
-    runner.run(settings)
-    runner.call_command("shell")
-
-@task
-def create_migration(name):
-    from d51.django.virtualenv.base import VirtualEnvironment
-    runner = VirtualEnvironment()
-    runner.run(settings)
-    runner.call_command("schemamigration", "embedded_video", name, initial=True)
