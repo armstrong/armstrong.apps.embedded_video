9462b76d11502783e7058497ff83ba0a33a43395
Travis Swicegood <development@domain51.com>
2011-05-10 15:50:46 -0500

    Modifying the migration (not-BC!)
    Obviously not the best way to do this, but rather than had edit a new
    migration for something that hasn't been merged into master, I'm just
    re-creating it with the new Content-based table structure.

--- armstrong/apps/embedded_video/migrations/0001_initial.py
+++ armstrong/apps/embedded_video/migrations/0001_initial.py
@@ -10,25 +10,20 @@ class Migration(SchemaMigration):
         
         # Adding model 'EmbeddedVideo'
         db.create_table('embedded_video_embeddedvideo', (
-            ('id', self.gf('django.db.models.fields.AutoField')(primary_key=True)),
-            ('pub_date', self.gf('django.db.models.fields.DateTimeField')(db_index=True)),
-            ('pub_status', self.gf('django.db.models.fields.CharField')(max_length=1)),
-            ('authors_extra', self.gf('django.db.models.fields.CharField')(max_length=200)),
-            ('authors_override', self.gf('django.db.models.fields.CharField')(max_length=200)),
+            ('content_ptr', self.gf('django.db.models.fields.related.OneToOneField')(to=orm['content.Content'], unique=True, primary_key=True)),
             ('video', self.gf('armstrong.core.arm_content.fields.video.EmbeddedVideoField')(max_length=255)),
-            ('title', self.gf('django.db.models.fields.CharField')(max_length=255)),
             ('aspect_ratio', self.gf('django.db.models.fields.CharField')(default='16:9', max_length=5)),
             ('screencap_url', self.gf('django.db.models.fields.URLField')(max_length=200)),
         ))
         db.send_create_signal('embedded_video', ['EmbeddedVideo'])
 
-        # Adding M2M table for field authors on 'EmbeddedVideo'
-        db.create_table('embedded_video_embeddedvideo_authors', (
+        # Adding M2M table for field sites on 'EmbeddedVideo'
+        db.create_table('embedded_video_embeddedvideo_sites', (
             ('id', models.AutoField(verbose_name='ID', primary_key=True, auto_created=True)),
             ('embeddedvideo', models.ForeignKey(orm['embedded_video.embeddedvideo'], null=False)),
-            ('user', models.ForeignKey(orm['auth.user'], null=False))
+            ('site', models.ForeignKey(orm['sites.site'], null=False))
         ))
-        db.create_unique('embedded_video_embeddedvideo_authors', ['embeddedvideo_id', 'user_id'])
+        db.create_unique('embedded_video_embeddedvideo_sites', ['embeddedvideo_id', 'site_id'])
 
 
     def backwards(self, orm):
@@ -36,8 +31,8 @@ class Migration(SchemaMigration):
         # Deleting model 'EmbeddedVideo'
         db.delete_table('embedded_video_embeddedvideo')
 
-        # Removing M2M table for field authors on 'EmbeddedVideo'
-        db.delete_table('embedded_video_embeddedvideo_authors')
+        # Removing M2M table for field sites on 'EmbeddedVideo'
+        db.delete_table('embedded_video_embeddedvideo_sites')
 
 
     models = {
@@ -70,6 +65,17 @@ class Migration(SchemaMigration):
             'user_permissions': ('django.db.models.fields.related.ManyToManyField', [], {'to': "orm['auth.Permission']", 'symmetrical': 'False', 'blank': 'True'}),
             'username': ('django.db.models.fields.CharField', [], {'unique': 'True', 'max_length': '30'})
         },
+        'content.content': {
+            'Meta': {'object_name': 'Content'},
+            'authors': ('armstrong.core.arm_content.fields.authors.AuthorsField', [], {'to': "orm['auth.User']", 'override_field_name': "'authors_override'", 'extra_field_name': "'authors_extra'", 'symmetrical': 'False'}),
+            'authors_extra': ('django.db.models.fields.CharField', [], {'max_length': '200'}),
+            'authors_override': ('django.db.models.fields.CharField', [], {'max_length': '200'}),
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'pub_date': ('django.db.models.fields.DateTimeField', [], {'db_index': 'True'}),
+            'pub_status': ('django.db.models.fields.CharField', [], {'max_length': '1'}),
+            'summary': ('django.db.models.fields.TextField', [], {}),
+            'title': ('django.db.models.fields.CharField', [], {'max_length': '255'})
+        },
         'contenttypes.contenttype': {
             'Meta': {'ordering': "('name',)", 'unique_together': "(('app_label', 'model'),)", 'object_name': 'ContentType', 'db_table': "'django_content_type'"},
             'app_label': ('django.db.models.fields.CharField', [], {'max_length': '100'}),
@@ -80,15 +86,29 @@ class Migration(SchemaMigration):
         'embedded_video.embeddedvideo': {
             'Meta': {'object_name': 'EmbeddedVideo'},
             'aspect_ratio': ('django.db.models.fields.CharField', [], {'default': "'16:9'", 'max_length': '5'}),
-            'authors': ('armstrong.core.arm_content.fields.authors.AuthorsField', [], {'to': "orm['auth.User']", 'override_field_name': "'authors_override'", 'extra_field_name': "'authors_extra'", 'symmetrical': 'False'}),
-            'authors_extra': ('django.db.models.fields.CharField', [], {'max_length': '200'}),
-            'authors_override': ('django.db.models.fields.CharField', [], {'max_length': '200'}),
-            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
-            'pub_date': ('django.db.models.fields.DateTimeField', [], {'db_index': 'True'}),
-            'pub_status': ('django.db.models.fields.CharField', [], {'max_length': '1'}),
+            'content_ptr': ('django.db.models.fields.related.OneToOneField', [], {'to': "orm['content.Content']", 'unique': 'True', 'primary_key': 'True'}),
             'screencap_url': ('django.db.models.fields.URLField', [], {'max_length': '200'}),
-            'title': ('django.db.models.fields.CharField', [], {'max_length': '255'}),
+            'sites': ('django.db.models.fields.related.ManyToManyField', [], {'to': "orm['sites.Site']", 'symmetrical': 'False'}),
             'video': ('armstrong.core.arm_content.fields.video.EmbeddedVideoField', [], {'max_length': '255'})
+        },
+        'sites.site': {
+            'Meta': {'ordering': "('domain',)", 'object_name': 'Site', 'db_table': "'django_site'"},
+            'domain': ('django.db.models.fields.CharField', [], {'max_length': '100'}),
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'name': ('django.db.models.fields.CharField', [], {'max_length': '50'})
+        },
+        'taggit.tag': {
+            'Meta': {'object_name': 'Tag'},
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'name': ('django.db.models.fields.CharField', [], {'max_length': '100'}),
+            'slug': ('django.db.models.fields.SlugField', [], {'unique': 'True', 'max_length': '100', 'db_index': 'True'})
+        },
+        'taggit.taggeditem': {
+            'Meta': {'object_name': 'TaggedItem'},
+            'content_type': ('django.db.models.fields.related.ForeignKey', [], {'related_name': "'taggit_taggeditem_tagged_items'", 'to': "orm['contenttypes.ContentType']"}),
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'object_id': ('django.db.models.fields.IntegerField', [], {'db_index': 'True'}),
+            'tag': ('django.db.models.fields.related.ForeignKey', [], {'related_name': "'taggit_taggeditem_items'", 'to': "orm['taggit.Tag']"})
         }
     }
 
